fce51757647e90c342a7e04af00894d187e92406
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-12 17:37:06 +0100

    Nézetek betöltésének implementálása

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
 <?php
 
+use function EcoDrive\Environment\appConfig;
+
 require_once "./app/boot.php";
 require_once "./app/routing.php";
+require_once "./config.php";
+
+// Lehetővé teszi a nézetek betöltését és változók átadását az összes végpont számára
+function view(string $viewName, $data = null) {
+    $path = appConfig()->VIEWS_PATH . "/" . $viewName . ".php";
+
+    // Definiáljuk a változókat, hogy a nézetben elérhetőek legyenek. Ha string típusú, akkor elkódoljuk, hogy az
+    // XSS támadások ellen védekezzünk.
+    if (isset($data)) {
+        foreach ($data as $varName => $value) {
+            global ${$varName};
+
+            if (gettype($value) === "string")
+                ${$varName} = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
+
+            else
+                ${$varName} = $value;
+        }
+    }
+
+    return include $path;
+}
+
+// Kérelem kezelése
 
 $uri = parse_url($_SERVER["REQUEST_URI"]);
-$endpoint = EcoDrive\Routing\endpointForPath($uri["path"]);
+$endpoint = \EcoDrive\Routing\endpointForPath($uri["path"]);
 
 if (!isset($endpoint))
     http_response_code(404);
