8616b8aa65f836b79d8a876af83a448f14d4e626
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-12 21:12:20 +0100

    Kozmetikai változtatások

--- app/routing.php
+++ app/routing.php
@@ -1,39 +1,11 @@
 <?php
 
-namespace EcoDrive\Routing;
+namespace EcoDrive\Routing {
 
 use function EcoDrive\Environment\appConfig;
 
 require_once "config.php";
 require_once appConfig()->APP_ROOT."/endpoints/Authentication.php";
-
-function & _routingTable() {
-    static $routingTable = [];
-    return $routingTable;
-}
-
-function & _routeNameTable() {
-    static $names = [];
-    return $names;
-}
-
-function endpointForPath(string $route) {
-    if (!array_key_exists($route, _routingTable()))
-        return null;
-
-    return _routingTable()[$route];
-}
-
-function registerRoute(string $method, string $route, string $endpoint, string $handler, string $name = null) {
-    if (!array_key_exists($route, _routingTable()))
-        _routingTable()[$route] = [ $method => [$endpoint, $handler] ];
-
-    else
-        _routingTable()[$route][$method] =[ $endpoint, $handler];
-
-    if (isset($name))
-        _routeNameTable()[$name] = appConfig()->SERVER_ADDR . $route;
-}
 
 function registerAppRoutes() {
     // Itt lehet regisztrálni a végpontokat
@@ -42,9 +14,41 @@ function registerAppRoutes() {
     registerRoute("POST", "/auth/login", \EcoDrive\Endpoints\Authenticator::class, "doLogin", "login");
 }
 
-function route($name) {
-    if (!array_key_exists($name, _routeNameTable()))
+namespace Internal {
+    function & routingTable() {
+        static $routingTable = [];
+        return $routingTable;
+    }
+
+    function & routeNameTable() {
+        static $names = [];
+        return $names;
+    }
+}
+
+function endpointForPath(string $route) {
+    if (!array_key_exists($route, \Internal\routingTable()))
         return null;
 
-    return _routeNameTable()[$name];
+    return \Internal\routingTable()[$route];
+}
+
+function registerRoute(string $method, string $route, string $endpoint, string $handler, ?string $name = null) {
+    if (!array_key_exists($route,\Internal\routingTable()))
+        \Internal\routingTable()[$route] = [ $method => [$endpoint, $handler] ];
+
+    else
+        \Internal\routingTable()[$route][$method] =[ $endpoint, $handler];
+
+    if (isset($name))
+        \Internal\routeNameTable()[$name] = appConfig()->SERVER_ADDR . $route;
+}
+
+function route($name) {
+    if (!array_key_exists($name, \Internal\routeNameTable()))
+        return null;
+
+    return \Internal\routeNameTable()[$name];
+}
+
 }
