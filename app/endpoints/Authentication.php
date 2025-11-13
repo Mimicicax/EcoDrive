ccdfe49f79ae29b1e38ea62929980734c921afb1
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-13 12:48:59 +0100

    Bejelentkezéstől átirányít a főoldalra, ha a felhasználó autentikálva van

--- app/endpoints/Authentication.php
+++ app/endpoints/Authentication.php
@@ -2,15 +2,22 @@
 
 namespace EcoDrive\Endpoints;
 
+use EcoDrive\Models\Session;
 use function EcoDrive\Environment\appConfig;
+use function EcoDrive\Helpers\redirect;
 
 require_once "config.php";
 require_once appConfig()->APP_ROOT . "/endpoints/Endpoint.php";
+require_once appConfig()->APP_ROOT . "/models/Session.php";
+require_once appConfig()->APP_ROOT . "/helpers/Redirect.php";
 
 class Authenticator implements Endpoint
 {
     // Megjeleníti a bejelentkezés oldalát
     public function showLogin() {
+        if (Session::isAuthenticated())
+            return redirect("home");
+        
         return view("login", [ "title" => "Bejelentkezés" ]);
     }
 
