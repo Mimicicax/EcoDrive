b522d9e0f92ddb2884a656d2fd340faf87d7432a
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-13 19:41:02 +0100

    A főoldal mostmár csak bejelentkezve érhető el, így a currentUser() sosem null

--- app/views/home.php
+++ app/views/home.php
@@ -1,4 +1,4 @@
 <?php use EcoDrive\Models\Session; ?>
 
 <h1>Főoldal</h1>
-<p>Felhasználó: <?= Session::currentUser()?->username ?> </p>
+<p>Felhasználó: <?= Session::currentUser()->username ?> </p>
