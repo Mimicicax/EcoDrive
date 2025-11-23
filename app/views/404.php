e6be80fd7d73d13eae83191de220d7fe6fa080c6
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-23 21:17:37 +0100

    404-es oldal

---
+++ app/views/404.php
@@ -0,0 +1,6 @@
+<div id="container-404">
+    <h1>404</h1>
+    <h2>A keresett oldal nem található</h2>
+    <p>Lehet, hogy téves hivatozásra kattintott. Lentre kattintva visszatérhet a főoldalra.</p>
+    <a href="/" class="button primary">Vissza</a>
+</div>
