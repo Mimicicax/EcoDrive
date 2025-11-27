174acca9210905deb52e125944f9b3ccd471dfac
Mimicicax <zsigmond.k.szabo@icloud.com>
2025-11-27 20:59:35 +0100

    A főoldal mostmár nem önálló, hanem átirányít

--- app/endpoints/Home.php
+++ app/endpoints/Home.php
@@ -4,6 +4,7 @@ namespace EcoDrive\Endpoints;
 
 use EcoDrive\Models\Session;
 use function EcoDrive\Environment\appConfig;
+use function EcoDrive\Helpers\redirect;
 
 require_once "config.php";
 require_once appConfig()->APP_ROOT . "/models/Session.php";
@@ -11,7 +12,7 @@ require_once appConfig()->APP_ROOT . "/models/Session.php";
 class Home implements Endpoint
 {
     public function show() {
-        return view("home");
+        return redirect("vehicles");
     }
 
     public static function requiresAuth(): bool {
