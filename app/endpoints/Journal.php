b6b129dcd9ff1355ab496646380975d69e012ffc
Mimicicax <36550578+Mimicicax@users.noreply.github.com>
2025-12-23 21:06:26 +0100

    Napló bejegyzés hozzáadása

--- app/endpoints/Journal.php
+++ app/endpoints/Journal.php
@@ -2,6 +2,9 @@
 
 namespace EcoDrive\Endpoints;
 
+use EcoDrive\Models\Session;
+use EcoDrive\Models\User;
+use EcoDrive\Models\Vehicle;
 use function EcoDrive\Routing\route;
 
 require_once "config.php";
@@ -10,15 +13,20 @@ class Journal implements Endpoint
 {
 
     public function show() {
-        $data = [
-            "title" => "Napló",
-            "activeNavLink" => route("journal")
-        ];
-
-        return view("journal", $data);
+        return $this->showRoutes();
     }
 
     public static function requiresAuth(): bool {
         return true;
+    }
+
+    private function showRoutes() {
+        $data = [
+            "title" => "Napló",
+            "activeNavLink" => route("journal"),
+            "userVehicles" => Vehicle::findAll(Session::currentUser())
+        ];
+
+        return view("journal", $data);
     }
 }
--- app/views/journal.php
+++ app/views/journal.php
@@ -1,1 +1,84 @@
-<h2>Út</h2>
+<?php use function EcoDrive\Routing\route ?>
+
+<h1>Utazási napló</h1>
+
+<div class="vehicle-container-header">
+    <button class="button primary" onclick="openModal(document.querySelector('#add-journal-entry-popup'))">
+        Bejegyzés hozzáadása
+    </button>
+</div>
+
+<dialog id="add-journal-entry-popup" 
+        oncancel="closeModal(document.querySelector('#add-journal-entry-popup'))">
+    <div class="card">
+        <p class="dialog-title">Bejegyzés hozzáadása</p>
+
+        <form action="<?= route("journal") ?>" method="POST">
+
+            <span class="input-group">
+                <label for="vehicle">Jármű</label>
+
+                <select name="vehicle" id="vehicle">
+                    <?php foreach ($userVehicles as $vehicle): ?>
+                        <option value="<?= $vehicle->id ?>">
+                            <?php $name = $vehicle->licensePlate  . " (" . $vehicle->brand . " " . $vehicle->model . ")" ?>
+                            <?= $name ?>
+                        </option>
+                    <?php endforeach ?>
+                </select>
+            </span>
+
+            <span class="input-group">
+                <label for="travel_start">Indulás időpontja</label>
+                <input type="datetime-local" name="travel_start" id="travel_start">
+            </span>
+
+            <span class="input-group">
+                <label for="distance">Távolság (km)</label>
+                <input type="text" name="distance" id="distance">
+            </span>
+
+            <p>Indulási hely</p>
+            <span class="dual-input-group">
+                <span class="input-group">
+                    <label for="from_zip">Irányítószám</label>
+                    <input type="number" name="from_zip" id="from_zip" min="0" max="9999">
+                </span>
+
+                <span class="input-group">
+                    <label for="from_city">Város</label>
+                    <input type="text" name="from_city" id="from_city">
+                </span>
+            </span>
+
+            <span class="input-group">
+                <label for="from_street">Utca</label>
+                <input type="text" name="from_street" id="from_street">
+            </span>
+
+            <p>Érkezési hely</p>
+            <span class="dual-input-group">
+                <span class="input-group">
+                    <label for="to_zip">Irányítószám</label>
+                    <input type="number" name="to_zip" id="to_zip" min="0" max="9999">
+                </span>
+                
+                <span class="input-group">
+                    <label for="to_city">Város</label>
+                    <input type="text" name="to_city" id="to_city">
+                </span>
+            </span>
+
+            <span class="input-group">
+                <label for="to_street">Utca</label>
+                <input type="text" name="to_street" id="to_street">
+            </span>
+
+            <input type="submit" value="Hozzáadás" class="button primary add-top-margin">
+        </form>
+    </div>
+</dialog>
+
+<?php if (!empty($errors)): ?>
+    <script>openModal(document.getElementById("add-journal-entry-popup"))</script>
+<?php endif ?>
