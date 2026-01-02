7987013a72133ab10fde9e657a44fb57f05246c4
Mimicicax <36550578+Mimicicax@users.noreply.github.com>
2026-01-02 21:41:02 +0100

    Statisztika lekérdezése

--- app/endpoints/Statistics.php
+++ app/endpoints/Statistics.php
@@ -2,25 +2,107 @@
 
 namespace EcoDrive\Endpoints;
 
+use DateTimeImmutable;
 use function EcoDrive\Environment\appConfig;
 use function EcoDrive\Routing\route;
 use EcoDrive\Models\Session;
+use EcoDrive\Models\Route;
 
 require_once "config.php";
 require_once appConfig()->APP_ROOT . "/models/Session.php";
+require_once appConfig()->APP_ROOT . "/models/Route.php";
 require_once appConfig()->APP_ROOT . "/routing.php";
+require_once appConfig()->APP_ROOT . "/helpers/LeastSquaresExtrapolator.php";
 
 class Statistics implements Endpoint
 {
+
+    private const euAverageMonthlyCo2Emission = 383333;
 
     public function show() {
         return $this->showStats();
     }
 
+    private function previousYearAggregate($previousYearRoutes, $month) {
+        $yearlyEmission = 0;
+        $totalDistance = 0;
+        $monthlyEmission = 0;
+
+        foreach ($previousYearRoutes as $route) {
+            $yearlyEmission += $route->emission;
+            $totalDistance += $route->distance;
+
+            if ($route->travelStart->format('n') == $month)
+                $monthlyEmission += $route->emission;
+        }
+        
+        return [
+            "previousYearMonthlyEmission" => $monthlyEmission,
+            "previousYearlyEmission" => $yearlyEmission,
+            "previousYearlyDistance" => $totalDistance
+        ];
+    }
+
+    private function retrieveStats() {
+        $month = date('n');
+        $year = date('Y');
+
+        $currentYearRoutes = Route::findAllForUser(Session::currentUser(), $year);
+        $previousYearRoutes = Route::findAllForUser(Session::currentUser(), $year - 1);
+        
+        if (empty($currentYearRoutes) && empty($previousYearRoutes))
+            return null;
+
+        $yearlyEmission = 0;
+        $monthlyEmission = 0;
+        $totalYearlyDistance = 0;
+        $totalMonthlyDistance = 0;
+        $previousMonthEmission = 0;
+        $extrapolator = new \EcoDrive\Helpers\Statistics\LeastSquaresExtrapolator();
+        $monthlyEmissionPerVehicle = [];
+
+        foreach ($currentYearRoutes as $route) {
+            $yearlyEmission += $route->emission;
+            $totalYearlyDistance += $route->distance;
+
+            $routeMonth = $route->travelStart->format('n');
+
+            $extrapolator->feed((float) $route->travelStart->format('z') + 1, $route->emission);
+
+            if ($routeMonth == $month) {
+                $monthlyEmission += $route->emission;
+                $totalMonthlyDistance += $route->distance;
+
+                if (\array_key_exists($route->vehicle->id, $monthlyEmissionPerVehicle))
+                    $monthlyEmissionPerVehicle[$route->vehicle->id]["emission"] += $route->emission;
+
+                else
+                    array_push($monthlyEmissionPerVehicle, [ "vehicle" => $route->vehicle, "emission" => $route->emission ]);
+
+            } else if ($month > 1 && $routeMonth == $month - 1)
+                $previousMonthEmission += $route->emission;
+        }
+
+        $extrapolator->finalise();
+
+        return \array_merge($this->previousYearAggregate($previousYearRoutes, $month), [
+            "yearlyEmission" => $yearlyEmission,
+            "monthlyEmission" => $monthlyEmission,
+            "monthlyDistance" => $totalMonthlyDistance,
+            "previousMonthEmission" => $previousMonthEmission,
+            "perVehicleMonthlyEmissionData" => $monthlyEmissionPerVehicle,
+            "averageEUMonthlyCO2Emission" => Statistics::euAverageMonthlyCo2Emission,
+            "yearlyDistance" => $totalYearlyDistance,
+            "predictedMonthlyConsumption" => $extrapolator->evaluate((new DateTimeImmutable($year . "-" . ($month == 12 ? "12" : $month) . "-" . ($month == 12 ? "31" : "01")))->format('z') + 1),
+            "predictedYearlyConsumption" => $extrapolator->evaluate((new DateTimeImmutable("$year-12-31"))->format('z') + 1)
+        ]);
+    }
+
     private function showStats() {
         return view("statistics", [
             "activeNavLink" => route("statistics"), 
-            "title" => "Statisztika"
+            "title" => "Statisztika",
+            "stats" => $this->retrieveStats()
         ]);
     }
 
