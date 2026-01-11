66737362d7717532f791f2ba39a48554592e7b32
Mimicicax <36550578+Mimicicax@users.noreply.github.com>
2026-01-11 16:51:19 +0100

    Keresőmező

--- app/views/admin.php
+++ app/views/admin.php
@@ -1,1 +1,15 @@
-<h1>Admin uwu</h1>
+<?php use function EcoDrive\Routing\route ?>
+
+<h1>Felhasználó-kezelés</h1>
+
+<div class="container-header">
+    <form action="<?= route("admin") ?>" method="GET">
+        <span id="user-search-bar">
+            <span class="input-group">
+                <label for="user">Felhasználó</label>
+                <input type="search" name="query" id="user" placeholder="Felhasználónév vagy email cím">
+            </span>
+            <button class="button primary">Keresés</button>
+        </span>
+    </form>
+</div>
