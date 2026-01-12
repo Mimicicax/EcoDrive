1303f4e2a293537309e9dfaf33802e6a976ddc28
Mimicicax <36550578+Mimicicax@users.noreply.github.com>
2026-01-12 15:30:05 +0100

    Felhasználóadatok módosítása

--- app/endpoints/Admin.php
+++ app/endpoints/Admin.php
@@ -36,7 +36,31 @@ class Admin implements Endpoint
         else
             $queriedUser = User::find($query, User::FIND_BY_USERNAME);
 
-        return $this->showView([ "queriedUser" => $queriedUser, "query" => $query ]);
+        return $this->showView([ "query" => $query ], $queriedUser);
+    }
+
+    public function editUser() {
+        if (!Session::currentUser()->isAdmin)
+            return redirect("home");
+
+        $userId = trim($_POST["user"] ?? "");
+
+        if (!isset($userId) || filter_var($userId, FILTER_VALIDATE_INT) === false) {
+            http_response_code(404);
+            return $this->showView(errors: [ "updateFailed" => true ]);
+        }
+
+        $user = User::findById((int) $userId);
+
+        if (!isset($user)) {
+            http_response_code(404);
+            return $this->showView(errors: [ "updateFailed" => true ]);
+        }
+
+        if (isset($_POST["action"]) && $_POST["action"] == "delete")
+            return $this->deleteUser($user);
+
+        return $this->updateUserInfo($user);
     }
 
     public static function requiresAuth(): bool {
@@ -47,11 +71,76 @@ class Admin implements Endpoint
         return true;
     }
 
-    private function showView(array $data = []) {
+    private function updateUserInfo(User $user) {
+        $username = null;
+        $email = null;
+        $password = null;
+        $errors = [];
+
+        if (isset($_POST["username"]) && $_POST["username"] !== "") {
+            $username = trim($_POST["username"]);
+
+            if ($username === $user->username)
+                $username = null;
+
+            else if ($e = \EcoDrive\Helpers\validateUsername($username))
+                $errors["usernameError"] = $e;
+        }
+
+        if (isset($_POST["email"]) && $_POST["email"] !== "") {
+            $email = trim($_POST["email"]);
+
+            if ($email === $user->email)
+                $email = null;
+
+            else if ($e = \EcoDrive\Helpers\validateEmail($email))
+                $errors["emailError"] = $e;
+        }
+
+        if (isset($_POST["newPassword"]) && $_POST["newPassword"] !== "") {
+            $new = $_POST["newPassword"] ?? "";
+            $confirm = $_POST["confirmPassword"] ?? "";
+
+            if ($e = \EcoDrive\Helpers\validatePassword($new, $confirm))
+                $errors["newPasswordError"] = $e;
+
+            $password = $_POST["newPassword"];
+        }
+
+        if (!empty($errors)) {
+            return $this->showView([
+                "providedUsername" => $_POST["username"] ?? "",
+                "providedEmail" => $_POST["email"] ?? "",
+                "providedNewPassword" => $_POST["newPassword"] ?? "",
+                "providedConfirmPassword" => $_POST["confirmPassword"] ?? ""
+
+            ], $user, $errors);
+        }
+
+        $queryName = isset($username) ? $username : $user->username;
+        $user->username = $username;
+        $user->email = $email;
+        $user->password = $password;
+
+        if (!$user->update()) {
+            http_response_code(500);
+            return $this->showView(queriedUser: $user, errors: [ "updateFailed" => true ]);
+        }
+
+        return redirect(to: "admin", type: \EcoDrive\Helpers\RedirectType::SeeOther, additionalData: [ "query" => $queryName ]);
+    }
+
+    private function deleteUser(User $user) {
+
+    }
+
+    private function showView(array $data = [], ?User $queriedUser = null, ?array $errors = null) {
         return view("admin", [
             ...$data,
             "title" => "Adminisztráció",
+            "queriedUser" => $queriedUser,
             "activeNavLink" => route("admin")
-        ]);
+
+        ], $errors);
     }
 }
